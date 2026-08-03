1acd9db4af987806e913e3efe2ad885b21dc02f9
adit <pradityamahaw@gmail.com>
2026-08-04 01:39:27 +0800

    fix: update connection.js for Atlas standard connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,4 @@
 import mongoose from "mongoose";
-import dns from "dns";
-
-try {
-  dns.setServers(["8.8.8.8", "1.1.1.1"]);
-} catch (e) {
-  // ignore if dns servers cannot be overridden
-}
 
 const connect = async (req, res) => {
   try {
