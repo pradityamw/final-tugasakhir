c0b6c3a158a5200d6d8e063303a561bdd9384089
adit <pradityamahaw@gmail.com>
2026-08-04 02:00:26 +0800

    fix: add override: true to force reload of .env variables

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, ".env") });
+dotenv.config({ path: path.join(__dirname, ".env"), override: true });
 
 import bodyParser from "body-parser";
 import express from "express";
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, ".env") });
+dotenv.config({ path: path.join(__dirname, ".env"), override: true });
 
 import app from "./app.js";
 import connect from "./config/connection.js";
